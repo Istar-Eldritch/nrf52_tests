bb3c09157b97e121e8ee652393ed33b933c1a774
Ruben Paz <me@ruben.io>
2019-03-08 18:58:20 +0000

    Use the board timer

--- examples/blinky.rs
+++ examples/blinky.rs
@@ -1,43 +1,31 @@
 #![no_std]
 #![no_main]
 
-extern crate cortex_m;
 extern crate cortex_m_rt as rt;
-
-extern crate embedded_hal_spy;
 extern crate nrf52832_hal as hal;
-extern crate cortex_m_semihosting as sh;
 extern crate panic_halt;
 extern crate nrf52_tests;
+extern crate nb;
 
 use rt::entry;
-use cortex_m::peripheral::syst;
+use hal::prelude::*;
+use nb::block;
 
 #[entry]
 fn main() -> ! {
     let mut board = nrf52_tests::Board::take().unwrap();
-    board.leds.led_1.enable();
 
-
-    let mut systick = board.SYST;
-
-    systick.set_clock_source(syst::SystClkSource::Core);
-
-    // 32Hz 
-    systick.set_reload(32_000_000);
-
-    systick.enable_counter();
+    let mut timer = board.TIMER0.constrain();
 
     let mut led_is_on = false;
     loop {
-        while !systick.has_wrapped() {}
         if led_is_on {
             board.leds.led_1.disable();
         } else {
             board.leds.led_1.enable();
         }
-        systick.clear_current();
-
+        timer.start(1_000_000_u32);
+        block!(timer.wait());
         led_is_on = !led_is_on;
     }
 }
